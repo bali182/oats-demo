de40c8fa87e67072d5e04bb80dd9ace62e9dad3e
Balázs Édes <edesbalazs@gmail.com>
2021-08-29 16:29:22 +0200

    Added deps for testing

---
+++ src/demo.test.ts
@@ -0,0 +1,6 @@
+describe("Playground for oats demo", () => {
+  it("should test Book instance with type guard", () => {});
+  it("should validate Book instance with validator", () => {});
+  it("should request Books using operation", async () => {});
+  it("should request Books using API", async () => {});
+});
